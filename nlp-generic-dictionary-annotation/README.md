ccc4328f6c2d7cb95c281925d7793617f5c9e20e
Javi Corvi <jcorvi@loyal-solutions.com>
2019-06-04 17:49:36 +0200

    TRELLO#71 Generic Dictionary Annotation Component

---
+++ nlp-generic-dictionary-annotation/README.md
@@ -0,0 +1,45 @@
+nlp-generic-dictionary-annotation
+========================
+
+<b>Generic library for dictionary mapping in text</b>   
+
+========================
+
+This library annotated text with terms present in a dictionary using the GATE format.  Is a wrapper that execute the ANNIE DefaultGazeteer and basics JAPE rules that annotated text with given dictionaries. 
+
+One of the input is the list of the dictionaries/gazeteers entries, has to be provided as in the GATE DefaultGazzeteer format. More information here:
+https://gate.ac.uk/sale/tao/splitch13.html#x18-32200013.2
+
+After the execution of the DefaultGazetter, the tool executes a JAPE rule to detect the Lookup annotations and generate the corresponding annotation taking into account the minorType of the configuration, and copy all the features present in the Lookup annotation. 
+For example:
+
+hepatotoxicity.lst:TOXICOLOGY:HEPATOTOXICITY  
+
+The name of the annotation will be HEPATOTOXICITY and is going to be set into the annotationSet given as parameter. 
+This tool remove the Basic Lookup annotation from the DefaultGazeteer.
+
+This library is very use full if we need to run batch mode, generating the software container.
+
+========================
+
+Build and run the docker individually
+
+	# To build the docker, just go into the ades-tagger folder and execute
+	docker build -t nlp-generic-dictionary-annotation .
+	#To run the docker, just set the input_folder and the output
+	mkdir ${PWD}/ades_output; docker run --rm -u $UID -v ${PWD}/input_output:/in:ro -v ${PWD}/ades_output:/out:rw nlp-generic-dictionary-annotation nlp-generic-dictionary-annotation -i /in -o /out	
+Parameters:
+<p>
+-i input folder with the documents to annotated. The documents could be plain txt or xml gate documents.
+</p>
+<p>
+-o output folder with the documents annotated in gate format.
+</p>
+<p>
+-a annotation set output
+</p>
+<p>
+-l list definition of the dictionary, with the GATE format.
+</p>
+		
+		
---
+++ nlp-generic-dictionary-annotation/src/main/java/es/bsc/inb/nlp/generic/dictionary/annotation/main/App.java
@@ -0,0 +1,194 @@
+package es.bsc.inb.nlp.generic.dictionary.annotation.main;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import gate.Corpus;
+import gate.Document;
+import gate.Factory;
+import gate.FeatureMap;
+import gate.Gate;
+import gate.LanguageAnalyser;
+import gate.ProcessingResource;
+import gate.creole.Plugin;
+import gate.creole.SerialAnalyserController;
+import gate.util.ExtensionFileFilter;
+import gate.util.GateException;
+
+/**
+ * Generic Library for Dictionary based annotation in Gate.
+ *
+ */
+public class App {
+    public static void main( String[] args ){
+    	Options options = new Options();
+    	
+        Option input = new Option("i", "input", true, "input directory path");
+        input.setRequired(true);
+        options.addOption(input);
+        
+        Option output = new Option("o", "output", true, "output directory path");
+        output.setRequired(true);
+        options.addOption(output);
+        
+        Option listDefinitions = new Option("l", "lists_definitions", true, "Dictionary List definitions, Gate format");
+        listDefinitions.setRequired(true);
+        options.addOption(listDefinitions);
+     
+        Option set = new Option("a", "annotation_set", true, "Annotation set where the annotation will be included");
+        set.setRequired(true);
+        options.addOption(set);
+        
+        Option workdir = new Option("w", "workdir", true, "workDir directory path");
+        workdir.setRequired(false);
+        options.addOption(workdir);
+        
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+    	try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+            System.exit(1);
+        }
+    	
+        String inputFilePath = cmd.getOptionValue("input");
+        String outputFilePath = cmd.getOptionValue("output");
+        String workdirPath = cmd.getOptionValue("workdir");
+        String annotationSet = cmd.getOptionValue("annotation_set");
+        String listsDefinitionsPath = cmd.getOptionValue("lists_definitions");
+        if (!java.nio.file.Files.isDirectory(Paths.get(inputFilePath))) {
+    		System.out.println(" Please set the inputDirectoryPath ");
+			System.exit(1);
+    	}
+        
+        if (!java.nio.file.Files.isRegularFile(Paths.get(listsDefinitionsPath))) {
+    		System.out.println(" Please set the list of dictionaries to annotate");
+			System.exit(1);
+    	}
+        
+        if (annotationSet==null) {
+        	System.out.println("Please set the annotation set where the annotation will be included");
+			System.exit(1);
+    	}
+    	
+    	File outputDirectory = new File(outputFilePath);
+	    if(!outputDirectory.exists())
+	    	outputDirectory.mkdirs();
+	    
+	    try {
+			Gate.init();
+		} catch (GateException e) {
+			System.out.println("App :: main :: Gate Exception  ");
+			e.printStackTrace();
+			System.exit(1);
+		} 
+ 
+	    if(workdirPath==null) {
+	    	workdirPath="";
+	    }
+
+	    try {
+			process(inputFilePath, outputFilePath, listsDefinitionsPath, annotationSet);
+		} catch (GateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+    }
+    
+    /**
+     * Annotation Process
+     * @param inputDirectory
+     * @param outputDirectory
+     * @throws GateException
+     * @throws IOException
+     */
+    private static void process(String inputDirectory, String outputDirectory, String listsDefinitionsPath,String annotationSet) throws GateException, IOException {
+    	Corpus corpus = Factory.newCorpus("My XML Files"); 
+    	File directory = new File(inputDirectory); 
+    	ExtensionFileFilter filter = new ExtensionFileFilter("Txt files", new String[]{"txt","xml"}); 
+    	URL url = directory.toURL(); 
+    	corpus.populate(url, filter, null, false);
+    	
+    	Plugin anniePlugin = new Plugin.Maven("uk.ac.gate.plugins", "annie", "8.5"); 
+    	Gate.getCreoleRegister().registerPlugin(anniePlugin); 
+    	// create a serial analyser controller to run ANNIE with 
+    	SerialAnalyserController annieController =  (SerialAnalyserController) Factory.createResource("gate.creole.SerialAnalyserController",  
+    	      Factory.newFeatureMap(), Factory.newFeatureMap(), "ANNIE"); 
+    	   
+//    	  String[] PR_NAMES = {
+//    			       "gate.creole.annotdelete.AnnotationDeletePR",
+//    			      "gate.creole.tokeniser.DefaultTokeniser",
+//    			      "gate.creole.gazetteer.DefaultGazetteer",
+//    			     "gate.creole.splitter.SentenceSplitter",
+//    			    "gate.creole.POSTagger",
+//    			     "gate.creole.ANNIETransducer",
+//    			    "gate.creole.orthomatcher.OrthoMatcher"
+//    			  };
+//    	  
+//    	  for(int i = 0; i < PR_NAMES.length; i++) { 
+//    		  FeatureMap params = Factory.newFeatureMap(); 
+//    		  ProcessingResource pr = (ProcessingResource)Factory.createResource(PR_NAMES[i], params); 
+//    		  // add the PR to the pipeline controller 
+//    		  annieController.add(pr); 
+//    	  } // for each ANNIE PR 
+//    	  
+    	   
+    	annieController.setCorpus(corpus); 
+    	  
+    	FeatureMap params = Factory.newFeatureMap(); 
+    	try {
+    		params.put("listsURL", new File(listsDefinitionsPath).toURL());
+    	} catch (MalformedURLException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}
+    	
+    	params.put("gazetteerFeatureSeparator", "\t");
+    	ProcessingResource treatment_related_finding_gazetter = (ProcessingResource) Factory.createResource("gate.creole.gazetteer.DefaultGazetteer", params); 
+    	annieController.add(treatment_related_finding_gazetter);
+    	
+    try {
+		LanguageAnalyser jape = (LanguageAnalyser)gate.Factory.createResource("gate.creole.Transducer", gate.Utils.featureMap(
+		              "grammarURL", new File("jape_rules/basic_mapping.jape").toURI().toURL(),"encoding", "UTF-8"));
+		jape.setParameterValue("outputASName", annotationSet);
+		annieController.add(jape);
+		// Run ANNIE 
+    	annieController.execute();
+    	
+	} catch (MalformedURLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+    for (Document  document : corpus) {
+    	java.io.Writer out = new java.io.BufferedWriter(new java.io.OutputStreamWriter(new FileOutputStream(new File(outputDirectory + File.separator +document.getName()), false)));
+	    out.write(document.toXml());
+	    out.close();
+	}
+    	 
+    System.out.println("App :: main :: END PROCESS");
+    	  
+		
+	}
+
+    
+}
